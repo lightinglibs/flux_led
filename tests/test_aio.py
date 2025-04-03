e107a88594ecdba91462f5817d2b0c1a2a2d7bd4
J. Nick Koston <nick@koston.org>
2025-04-03 11:11:06 -1000

    still not right but closer

--- tests/test_aio.py
+++ tests/test_aio.py
@@ -3888,6 +3888,9 @@ def test_extended_state_to_state_full_cool_white():
     # Validate fields
     assert raw_state.power_state == 0x23  # power
     assert raw_state.preset_pattern == 0x61  # preset
+    assert raw_state.red == 0  # red
+    assert raw_state.green == 0  # green
+    assert raw_state.blue == 0  # blue
     assert raw_state.warm_white == 0  # warm white
     assert raw_state.cool_white == 255  # cool white
 
@@ -3932,7 +3935,57 @@ def test_extended_state_to_state_full_warm_white():
     # Validate fields
     assert raw_state.power_state == 0x23  # power
     assert raw_state.preset_pattern == 0x61  # preset
+    assert raw_state.red == 0  # red
+    assert raw_state.green == 0  # green
+    assert raw_state.blue == 0  # blue
     assert raw_state.warm_white == 255  # warm white
+    assert raw_state.cool_white == 0  # cool white
+
+
+def test_extended_state_to_state_full_red():
+    proto = ProtocolLEDENET25Byte()
+
+    # Simulated extended state response payload (starts with EA 81)
+    raw_state = bytes(
+        (
+            0xEA,
+            0x81,
+            0x01,
+            0x00,
+            0x35,
+            0x0A,
+            0x23,
+            0x61,
+            0x00,
+            0x0A,
+            0xF0,
+            0x00,
+            0x64,
+            0x64,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0xF8,
+        )
+    )
+
+    assert proto.is_valid_extended_state_response(raw_state) is True
+
+    state = proto.extended_state_to_state(raw_state)
+    assert len(state) == 14
+
+    raw_state = LEDENETRawState(*state)
+
+    # Validate fields
+    assert raw_state.power_state == 0x23  # power
+    assert raw_state.preset_pattern == 0x61  # preset
+    assert raw_state.red == 255  # red
+    assert raw_state.green == 0  # green
+    assert raw_state.blue == 0  # blue
+    assert raw_state.warm_white == 0  # warm white
     assert raw_state.cool_white == 0  # cool white
 
 
